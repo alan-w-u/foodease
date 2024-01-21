a818fc737e08e11b4ee4cfcd444ab05b31ba9123
Papershine <hilarylau119@gmail.com>
2024-01-21 08:51:13 -0800

    add restaurant info

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,18 +1,28 @@
 import { useState, useEffect } from "react";
 import { getRestaurants } from "../googlemaps";
 import { googleKey } from "../secrets";
+import { Button } from "primereact/button"
 import "./Restaurant.css";
 
 function Restaurant(props) {
   var [loaded, setLoaded] = useState(false);
   var [name, setName] = useState("");
-  var [photo, setPhoto] = useState("")
+  var [photo, setPhoto] = useState("");
+  var [rating, setRating] = useState("");
+  var [ratingCount, setRatingCount] = useState("");
+  var [address, setAddress] = useState("");
+  var [link, setLink] = useState("");
+
   useEffect(() => {
     getRestaurants(props.cuisine, props.dish, props.location).then((res) => {
       setName(res[0].displayName.text);
       setPhoto(res[0].photos[0]["name"]);
+      setRating(res[0].rating);
+      setRatingCount(res[0].userRatingCount);
+      setAddress(res[0].formattedAddress);
+      setLink(res[0].googleMapsUri);
       setLoaded(true);
-      props.callback(res[0].displayName.text);
+      props.callback(res[0].displayName.text + " " + res[0].formattedAddress);
     })
   }, []);
 
@@ -20,10 +30,16 @@ function Restaurant(props) {
     <>
       {
         loaded ? 
-        <>
-          <p id="restaurant-name">{name}</p>
+        <div className="restaurantCard">
+          <div className="restaurantInfo">
+            <p id="restaurant-name">{name}</p>
+            <p>{address}</p>
+            <p>&#9734; {rating} ({ratingCount})</p>
+            <Button label="Get Directions" onClick={() => document.getElementById("directions").scrollIntoView(true)}/>
+            <Button label="Open in Google Maps" onClick={() => window.open(link, '_blank')}/>
+          </div>
           <img className="restaurantPhoto" src={"https://places.googleapis.com/v1/" + photo + "/media?maxWidthPx=800&key=" + googleKey} alt="restaurant"></img>
-        </> : <></>
+        </div> : <></>
       }
     </>
   )
