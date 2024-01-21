19cc066c0af97f08e1853e38250f87140d53531b
alllanwu <alanwu0004@gmail.com>
2024-01-20 17:11:31 -0800

    Edited front-end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import downarrow from './downarrow.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <section id="home">
+      <div className="blur center">
+        <h1 className="shadow">Foodease</h1>
+        <a href="#filters" id="start" className="shadow"><p>Where to Go?</p></a>
+        <img src={downarrow} href="#filters" className="downarrow" alt="downarrow" />
+      </div>
+      </section>
+      <section id="filters">
+        <h2>Filters</h2>
+        <div>
+          <p>Weather:</p>
+          <p>Temperature:</p>
+          <p>Location:</p>
+          <p>Background:</p>
+        </div>
+      </section>
+      <section>
+        <h2></h2>
+      </section>
+      <section>
+        <h2></h2>
+      </section>
+      </div>
   );
 }
 
